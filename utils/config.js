1ad3d8a5606ce772331479b9252071c041ea6ed5
Maksim Khier <maksimkhier@outlook.com>
2018-07-01 02:00:30 +0800

    forgot to change api_client

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   appName: 'ToyHouse',
-  //baseUrl: 'https://toyhouse.dokku.mutan.io/api/v1'
-  baseUrl: 'http://localhost:3000/api/v1'
+  baseUrl: 'https://toyhouse.dokku.mutan.io/api/v1'
+  //baseUrl: 'http://localhost:3000/api/v1'
 };
